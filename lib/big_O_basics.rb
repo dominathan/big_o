c6cb1dc1f2a14033e31c821e6497b2b81da544c3
Nathan Hall <nathan.mh@gmail.com>
2015-01-16 17:17:38 -0600

    merge sort next

--- lib/big_O_basics.rb
+++ lib/big_O_basics.rb
@@ -1,6 +1,21 @@
 require 'pry'
 require 'benchmark'
+
 class BigO
+  attr_accessor :my_array1, :my_array2, :my_array4,:my_array3,
+                :my_array5,:my_array6,:my_array7,:my_array8,:my_array9
+
+  def initialize
+    @my_array1 = (1..10)
+    @my_array2 = (1..100)
+    @my_array3 = (1..1000)
+    @my_array4 = (1..10000)
+    @my_array5 = (1..100000)
+    @my_array6 = (1..1000000)
+    @my_array7 = (1..10000000)
+    @my_array8 = (1..100000000)
+    @my_array9 = (1..1000000000)
+  end
 
   #constant time O(1)
   def self.constant_time(input,value)
@@ -38,3 +53,14 @@ class BigO
   #O(log(N))
   #see binary_search.rb
 end
+
+big = BigO.new
+# Benchmark.measure { BigO.linear_time(big.my_array1, "hello") }
+# Benchmark.measure { BigO.linear_time(big.my_array2, "hello") }
+# Benchmark.measure { BigO.linear_time(big.my_array3, "hello") }
+# Benchmark.measure { BigO.linear_time(big.my_array4, "hello") }
+# Benchmark.measure { BigO.linear_time(big.my_array5, "hello") }
+# Benchmark.measure { BigO.linear_time(big.my_array6, "hello") }
+# Benchmark.measure { BigO.linear_time(big.my_array7, "hello") }
+# #Benchmark.measure { BigO.linear_time(big.my_array8, "hello") }
+#Benchmark.measure { BigO.linear_time(big.my_array9, "hello") }
--- lib/binary_search.rb
+++ lib/binary_search.rb
@@ -1,8 +1,8 @@
 #given a sorted array, find the element
 require 'pry'
 require 'benchmark'
-class Search
 
+class Search
   attr_accessor :my_array1, :my_array2, :my_array4,:my_array3,
                 :my_array5,:my_array6,:my_array7,:my_array8,:my_array9
 
@@ -13,13 +13,13 @@ class Search
     @my_array4 = (1..10000).to_a
     @my_array5 = (1..100000).to_a
     @my_array6 = (1..1000000).to_a
-    #@my_array7 = (1..10000000).to_a
-    # @my_array8 = (1..100000000).to_a
-    # @my_array9 = (1..1000000000).to_a
+    #@my_array7 = (1..10000000)
+    #@my_array8 = (1..100000000)
+    #@my_array9 = (1..1000000000)
   end
 
   def binary_search(elem,array)
-    middle_index = (array.length/2).floor
+    middle_index = (array.size/2).floor
     if array[middle_index] == elem
       return array[middle_index]
     elsif array[middle_index] < elem
