f529ffcfa555d7554faa005ab1de4a8226906e6c
Nathan Hall <nathan.mh@gmail.com>
2015-01-16 13:00:02 -0600

    binary serach and bigO starting points

---
+++ lib/big_O_basics.rb
@@ -0,0 +1,15 @@
+class BigO
+
+  #constant time O(1)
+  def constant_time(input)
+    if input[0] == "HELLO"
+      return true
+    else
+      return false
+    end
+  end
+
+  #O(N)
+  def
+  end
+end
