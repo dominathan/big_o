3a068c53b8a3ed7fcc69ea4fd27a94ee22a7d3be
Nathan Hall <nathan.mh@gmail.com>
2015-01-16 14:18:36 -0600

    BigO explanations

--- lib/big_O_basics.rb
+++ lib/big_O_basics.rb
@@ -1,8 +1,10 @@
+require 'pry'
+require 'benchmark'
 class BigO
 
   #constant time O(1)
-  def constant_time(input)
-    if input[0] == "HELLO"
+  def self.constant_time(input,value)
+    if input[0] == value
       return true
     else
       return false
@@ -10,6 +12,29 @@ class BigO
   end
 
   #O(N)
-  def
+  def self.linear_time(input, value)
+    value_location = "I'm not here"
+    input.each_with_index do |element_in_input, index|
+      if element_in_input == value.to_i
+        value_location = "I'm at position #{index}"
+      end
+    end
+    puts value_location
   end
+
+  #O(N^2)
+  def self.exponential_time(input1,input2,value)
+    duplicate = "No duplicates"
+    input1.each do |elements_in_input_1|
+      input2.each do |elements_in_input_2|
+        if elements_in_input_2 == elements_in_input_1
+          duplicate = "DUPLICATE VALUE"
+        end
+      end
+    end
+    duplicate
+  end
+
+  #O(log(N))
+  #see binary_search.rb
 end
